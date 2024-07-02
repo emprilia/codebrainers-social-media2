493378e7219d8b965802d2d115f3aa1d85bf59ed
Miłosz Karwacki <m.karwacki94@gmail.com>
2024-07-02 18:26:43 +0200

    008 with bugs

--- assets/js/signUp/validators.js
+++ assets/js/signUp/validators.js
@@ -1,44 +1,31 @@
-import { displayError, removeError } from "./errors.js";
 import { passwordStrength } from "check-password-strength";
+import { displayError, removeError } from "./errors.js";
+
+export const checkValidatResult =
+    (input, field, contidion, massageError) => {
+        console.log(field);
+        if (contidion) {
+            displayError(input, massageError, field);
+        } else {
+            removeError(input, field);
+        }
+    };
 
 export const validateUsername = (input, field) => {
-    if (input.value.length < 4 || input.value.length > 16) {
-        displayError(input, "Username must be between 4 and 16 characters.");
-        field.isValid = false;
-    } else {
-        field.isValid = true;
-        removeError(input);
-    }
+    console.log(field);
+    checkValidatResult(input, field, input.value.length < 4 || input.value.length > 16, "Username must be between 4 and 16 characters.");
 };
 
 export const validateEmail = (input, field) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(input.value)) {
-        displayError(input, "Email is not valid.");
-        field.isValid = false;
-    } else {
-        field.isValid = true;
-        removeError(input);
-    }
+    checkValidatResult(input, field, !emailRegex.test(input.value), "Email is not valid.");
 };
 
 export const validatePassword = (input, field) => {
-    if (passwordStrength(input.value).id < 2) {
-        displayError(input, "Password is not strong enough.");
-        field.isValid = false;
-    } else {
-        field.isValid = true;
-        removeError(input);
-    }
+    checkValidatResult(input, field, passwordStrength(input.value).id < 2, "Password is not strong enough.");
 };
 
 export const validateConfirmPassword = (input, field) => {
     const password = document.getElementById("password").value;
-    if (input.value !== password) {
-        displayError(input, "Passwords do not match.");
-        field.isValid = false;
-    } else {
-        field.isValid = true;
-        removeError(input);
-    }
+    checkValidatResult(input, field, input.value !== password, "Passwords do not match.");
 };
